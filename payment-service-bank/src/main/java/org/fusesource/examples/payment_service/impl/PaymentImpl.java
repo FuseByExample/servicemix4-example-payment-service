ca7a0301d6982173ce718374fd4ba4290c3ad25f
Scott Cranton <scranton@redhat.com>
2012-12-04 16:58:37 -0500

    Updates to latest version of Fuse ESB, and update copyrights

---
+++ payment-service-bank/src/main/java/org/fusesource/examples/payment_service/impl/PaymentImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) Red Hat, Inc.
+ * http://www.redhat.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fusesource.examples.payment_service.impl;
+
+import org.fusesource.examples.payment_service.Payment;
+import org.fusesource.examples.payment_service.types.TransferRequest;
+import org.fusesource.examples.payment_service.types.TransferResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PaymentImpl implements Payment {
+    private static final Logger LOG = LoggerFactory.getLogger(PaymentImpl.class);
+
+    @Override
+    public TransferResponse transferFunds(TransferRequest payload) {
+        TransferResponse response = new TransferResponse();
+
+        LOG.info("Bank: transferred amount {} from {} to {}", new Object[]{payload.getAmount(), payload.getFrom(), payload.getTo()});
+
+        response.setReply("Bank: OK");
+
+        return response;
+    }
+
+    public void init() {
+        LOG.info("Bank: PaymentImpl started...");
+    }
+}
--- payment-service-client/src/main/java/org/fusesource/examples/payment_service/client/PaymentClient.java
+++ payment-service-client/src/main/java/org/fusesource/examples/payment_service/client/PaymentClient.java
@@ -1,25 +1,26 @@
 /*
- * Copyright 2012 FuseSource
+ * Copyright (C) Red Hat, Inc.
+ * http://www.redhat.com
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.fusesource.examples.payment_service.client;
+package org.fusesource.examples.payment_service.client;
 
-import com.fusesource.examples.payment_service.Payment;
-import com.fusesource.examples.payment_service.PaymentService;
-import com.fusesource.examples.payment_service.types.TransferRequest;
-import com.fusesource.examples.payment_service.types.TransferResponse;
+import org.fusesource.examples.payment_service.Payment;
+import org.fusesource.examples.payment_service.PaymentService;
+import org.fusesource.examples.payment_service.types.TransferRequest;
+import org.fusesource.examples.payment_service.types.TransferResponse;
 
 import java.net.MalformedURLException;
 import java.net.URL;
